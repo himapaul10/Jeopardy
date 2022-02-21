d67854d2af14c4fef0913f91296a8ed14da22e97
Shreya Patel <67444978+shreya-patell@users.noreply.github.com>
2022-02-20 22:56:49 -0500

    Update jeopardy.c

--- jeopardy.c
+++ jeopardy.c
@@ -1,5 +1,5 @@
 /*
- * Tutorial 3 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
  * Copyright (C) 2015, <GROUP MEMBERS>
  * All rights reserved.
@@ -32,23 +32,124 @@ int main(int argc, char *argv[])
     player players[NUM_PLAYERS];
     
     // Input buffer and and commands
-    char buffer[BUFFER_LEN] = { 0 };
+    //char buffer[BUFFER_LEN] = { 0 };
 
     // Display the game introduction and initialize the questions
     initialize_game();
 
     // Prompt for players names
-    
+    printf("This is Jeopardy \n");
+
     // initialize each of the players in the array
+    for(int i = 0; i < 4; i++) {
+        players[i].score = 0;
+
+        printf("Enter player name: ");
+        scanf("%s", (char *) &players[i].name);
+    }
 
     // Perform an infinite loop getting command input from users until game ends
-    while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
+    //while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
+    while(!answered_status())
     {
-        // Call functions from the questions and players source files
+        system("clear");
+
+        char selected_player[MAX_LEN] = "";
+        char selected_category[MAX_LEN] = "";
+        int selected_val = 0;
+
+        do {
+            if(strcmp(selected_player, "") != 0)
+                printf("The player %s was not found", selected_player);
+
+            printf("Enter first player's name: ");
+            scanf("%s", (char *) &selected_player);
+        } while(!player_exists(players, 4, selected_player));
+
+        do {
+            if(selected_val != 0)
+                printf("Invalid selection");
+
+            printf("Enter category: ");
+            getchar();
+            fgets((char *) selected_category, MAX_LEN, stdin);
+            strtok(selected_category, "\n");
+
+            printf("Enter: ");
+            scanf("%d", (int *) &selected_val);
+        } while(already_answered(selected_category, selected_val));
+
+
+        system("clear");
+        display_question(selected_category, selected_val);
+
+        char *answer[MAX_LEN] = {0};
+        getchar();
+        fgets((char *) answer, MAX_LEN, stdin);
+
+        char *tokenize_answer;
+        tokenize((char *) answer, &tokenize_answer);
+
+        if(tokenize_answer == NULL)
+            printf("Try again");
+        else if(valid_answer(selected_category, selected_val, tokenize_answer)) {
+            printf("Correct Answer!");
+            printf("%s gains %d points \n", selected_player, selected_val);
+            update_score(players, 4, selected_player, selected_val);
+        } else {
+            printf("Wrong Answer!");
+            int num = get_question_number(selected_category, selected_val);
+            printf("Correct answer was: %s", questions[num].answer);
+        }
+
+        track_answered(selected_category, selected_val);
+
+        // Call functansions from the questions and players source files
 
         // Execute the game until all questions are answered
 
         // Display the final results and exit
     }
+
+    show_results(players, 4);
+    getchar();
+
     return EXIT_SUCCESS;
+}
+
+
+void tokenize(char *input, char **tokens) {
+    char *str;
+
+    if((str = strtok(input, "")) != NULL)
+        if(strcmp(str, "who") != 0 && strcmp(str, "what") != 0)
+            return;
+
+    if((str = strtok(NULL, "")) != NULL)
+        if(strcmp(str, "is") != 0)
+            return;
+
+    *tokens = strtok(NULL, "\n");
+}
+
+void show_results(player *players, int num_players) {
+    int name = 0;
+    int score = 0;
+    int winner = 0;
+
+    for(int i = 0; i < num_players; i++) {
+        if((int) strlen(players[i].name) > name)
+            name = strlen(players[i].name);
+
+        if(players[i].score > score) {
+            score = players[i].score;
+            winner = i;
+        }
+    }
+
+    printf("Scores: \n");
+    for(int i = 0; i < num_players; i++)
+        printf("%*s: %d\n", name + 1, players[i].name, players[i].score);
+
+    printf("Winner: %s", players[winner].name);
 }
